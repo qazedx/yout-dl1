2a8719f9841bdf1db530e27f2f2cd22c2de7a241
Mindaugas Sasnauskas <mindaugas.s@wilibox.com>
2015-09-14 11:07:08 +0300

    orderBy working in controler

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -17,6 +17,12 @@ angular.module('youtApp')
     $scope.deleteVid = function (vid) {
       MyService.deleteVid(vid);
     }
+    $scope.predicate = 'obj.timestamp';
+      $scope.reverse = true;
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+      };
   })
   .controller('downloadVid', function ($rootScope, $scope, MyService) {
     $scope.downloadVid = function (vid) {
