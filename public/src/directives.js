d2ab0064e75e55bb134e83d32f58f8bc2d12d9b5
Mindaugas Sasnauskas <mindaugas.s@wilibox.com>
2015-09-15 15:48:49 +0300

    style and ng model updates

--- public/src/directives.js
+++ public/src/directives.js
@@ -1,28 +1,38 @@
 angular.module('youtApp')
-  .directive('dlField', function() {
+  .directive('dlField', function () {
     return {
       restrict: 'EA',
       templateUrl: 'views/dl-field.html',
       replace: false,
-      link: function($scope, element, attr) {
-        $('#idl-input').on('click', function() {
-            this.setSelectionRange(0, this.value.length) // select input on click
+      link: function ($scope, element, attr) {
+        $('#idl-input').on('click', function () {
+          this.setSelectionRange(0, this.value.length) // select input on click
         });
       }
     };
   })
-  .directive('list',['MyService',  function(MyService) {
+  .directive('list', ['MyService', function (MyService) {
     return {
       restrict: 'AE',
       templateUrl: 'views/list.html',
       replace: true,
-      link: function($scope, element, attr) {
+      link: function ($scope, element, attr) {
 
-        $scope.deleteVid = function(video_id){
+        $scope.addVid = function (video_id) {
+            console.log(video_id);
+          $('#' + video_id + ' button.addVid-btn').toggleClass('btn-primary');
+          $('#' + video_id + '.panel').toggleClass('panel-primary');
+        };
+        $scope.viewVid = function (video_id) {
+            console.log(video_id);
+          $('#' + video_id + '.panel').toggleClass('viewVid-single');
+        };
+
+        $scope.deleteVid = function (video_id) {
           console.log(video_id);
-          $('#'+video_id).html('');
-            MyService.deleteVid(video_id);
-        }
+          $('#' + video_id).html('');
+          MyService.deleteVid(video_id);
+        };
 
       }
     };
