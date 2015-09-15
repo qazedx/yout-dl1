f31955756a67bab8789e2a8b092beffe54cba819
Mindaugas Sasnauskas <mindaugas.s@wilibox.com>
2015-09-15 17:03:54 +0300

    vido html() hack; add to folder widget; add to array working

--- public/src/directives.js
+++ public/src/directives.js
@@ -11,21 +11,52 @@ angular.module('youtApp')
       }
     };
   })
+  .directive('add2list', ['MyService', function (MyService) {
+    return {
+      restrict: 'AE',
+      templateUrl: 'views/add2list.html',
+      replace: true,
+      link: function ($scope, element, attr) {
+
+      }
+    }
+  }])
   .directive('list', ['MyService', function (MyService) {
     return {
       restrict: 'AE',
       templateUrl: 'views/list.html',
       replace: true,
       link: function ($scope, element, attr) {
-
+        var vid_add_arr = []
         $scope.addVid = function (video_id) {
-            console.log(video_id);
-          $('#' + video_id + ' button.addVid-btn').toggleClass('btn-primary');
-          $('#' + video_id + '.panel').toggleClass('panel-primary');
+
+          console.log(video_id);
+          var panel = $('#' + video_id + '.panel');
+          if (panel.hasClass('panel-primary')) {
+            $('#' + video_id + ' button.addVid-btn').removeClass('btn-primary');
+            panel.removeClass('panel-primary');
+            var index = vid_add_arr.indexOf(video_id);
+            if (index > -1) {
+              vid_add_arr.splice(index, 1);
+            }
+          } else {
+            $('#' + video_id + ' button.addVid-btn').addClass('btn-primary');
+            panel.addClass('panel-primary');
+            vid_add_arr.push(video_id);
+          }
+
+
+          if(vid_add_arr.length == 0 && !$('.add2list').hasClass('hidden')){
+            $('.add2list').addClass('hidden');
+          }else{
+            $('.add2list').removeClass('hidden');
+          }
+          console.log(vid_add_arr);
         };
         $scope.viewVid = function (video_id) {
-            console.log(video_id);
+          console.log(video_id);
           $('#' + video_id + '.panel').toggleClass('viewVid-single');
+          $('#' + video_id + '.panel .panel-body').html('<video src="/vid/' + video_id + '.mp4" controls></video>')
         };
 
         $scope.deleteVid = function (video_id) {
